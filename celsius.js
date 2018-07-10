ba49253780b237710e4402c839b51d12d5c63ed7
PRASAD RAO.JAMMUNA <jammunaprasadrao.com>
2018-07-10 21:35:11 +0530

    adding celcius.js file

---
+++ celsius.js
@@ -0,0 +1,17 @@
+var score =100
+
+score = 130
+const bonus = 50
+
+var finalScore = (score+bonus)*1.8
+
+// console.log(finalScore); this is a comment
+
+let tempInFahrenheat  = 72
+
+// Do colcualtion
+
+// Tc = (Tf-32)*(5/9)
+
+let celcius = (tempInFahrenheat - 32)*5/9
+console.log(celcius);
